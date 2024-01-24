d40d40f2459c3cbd394bc2f64f8d5d3d9681be98
Reed Searle <csearle@cnm.edu>
2024-01-24 12:02:57 -0700

    Update for Overloaded methods and tests

--- src/main/java/edu/cnm/deepdive/shuffler/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/shuffler/Shuffler.java
@@ -12,10 +12,83 @@ public class Shuffler {
 
   public void shuffle(int[] data) {
     for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
-      int srcIndex = rng.nextInt(destIndex + 1);
+      int srcIndex    = rng.nextInt(destIndex + 1);
       int temp        = data[destIndex];
       data[destIndex] = data[srcIndex];
       data[srcIndex]  = temp;
     }
   }
+
+  public void shuffle(long[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex    = rng.nextInt(destIndex + 1);
+      long temp       = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
+  public void shuffle(boolean[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex    = rng.nextInt(destIndex + 1);
+      boolean temp    = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
+  public void shuffle(char[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex    = rng.nextInt(destIndex + 1);
+      char temp       = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
+  public void shuffle(short[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex    = rng.nextInt(destIndex + 1);
+      short temp      = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
+  public void shuffle(byte[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex    = rng.nextInt(destIndex + 1);
+      byte temp       = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
+  public void shuffle(float[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex    = rng.nextInt(destIndex + 1);
+      float temp      = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
+  public void shuffle(double[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex    = rng.nextInt(destIndex + 1);
+      double temp     = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
+  public void shuffle(Object[] data) {
+    for (int destIndex = data.length -1; destIndex > 0; destIndex--) {
+      int srcIndex = rng.nextInt(destIndex + 1);
+      Object temp        = data[destIndex];
+      data[destIndex] = data[srcIndex];
+      data[srcIndex]  = temp;
+    }
+  }
+
 }
